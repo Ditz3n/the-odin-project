b0bee189bfc3154426c07428e262eac1e40be7b7
Mads Dittmann Villadsen <mvmads@gmail.com>
2026-07-01 21:33:51 +0200

    feat: add grid select- and rendering function

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,5 +1,49 @@
 console.log("Hello World!");
 
+function selectGridSize() {
+    let userSelectedGridSize = 0;
+    let gridColumnCount = userSelectedGridSize;
+    let gridRowCount = userSelectedGridSize;
+
+    // Prompt user for grid size
+    do {
+        userSelectedGridSize = prompt("Which size grid do you want (1-100)?:");
+        if (userSelectedGridSize === null) break;
+        userSelectedGridSize = parseInt(userSelectedGridSize);
+        gridColumnCount = userSelectedGridSize;
+        gridRowCount = userSelectedGridSize;
+        console.log(`Selected grid size: ${gridColumnCount}x${gridRowCount}.`);
+    }
+    while (
+        isNaN(userSelectedGridSize)
+        || userSelectedGridSize < 1
+        || userSelectedGridSize > 100
+    );
+
+    // Add column and row count to grid container
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.style.setProperty("--grid-columns", gridColumnCount);
+    gridContainer.style.setProperty("--grid-rows", gridColumnCount);
+
+    // Remove previous grid elements
+    document.querySelectorAll(".grid-container div").forEach((arr) => {
+        arr.remove();
+    });
+
+    // Iterate new grid elements into grid container
+    for (let i = 0; i < gridColumnCount * gridRowCount; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.classList.add(`grid-element-${i}`);
+        gridContainer.appendChild(gridElement);
+    };
+
+    console.log(`A ${gridColumnCount}x${gridRowCount} grid container has been created!`);
+};
+
+function selectBrushColor() {
+
+};
+
 // Get all button elements from the DOM as a NodeList element
 const allBtnElements = document.querySelectorAll("button");
 
@@ -12,23 +56,24 @@ allBtnElements.forEach((arr) => {
     arr.addEventListener("click", () => {
         switch(arr.classList.value) {
             case("btn-one"):
-                console.log("Button 1 Clicked!")
+                console.log("Button 1 Clicked!");
+                selectGridSize();
                 break;
             case("btn-two"):
-                console.log("Button 2 Clicked!")
+                console.log("Button 2 Clicked!");
                 break;
             case("btn-three"):
-                console.log("Button 3 Clicked!")
+                console.log("Button 3 Clicked!");
                 break;
             case("btn-four"):
-                console.log("Button 4 Clicked!")
+                console.log("Button 4 Clicked!");
                 break;
             case("btn-five"):
-                console.log("Button 5 Clicked!")
+                console.log("Button 5 Clicked!");
                 break;
             case("btn-six"):
-                console.log("Button 6 Clicked!")
+                console.log("Button 6 Clicked!");
                 break;
-        }  
-    })
-})
+        };  
+    });
+});
